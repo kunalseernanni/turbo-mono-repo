8d18284fa3f9d679d71765984d759ebc257c1905
kunalseernanni <kunalseernani@gmail.com>
2025-03-24 20:30:07 +0100

    react app

---
+++ turbo-repo/apps/web/app/admin/page.tsx
@@ -0,0 +1,10 @@
+import { Admin } from "@repo/ui/admin";
+
+export default function(){
+    return (
+        <div>
+            <Admin></Admin>
+            hi from admin page
+        </div>
+    );
+}
